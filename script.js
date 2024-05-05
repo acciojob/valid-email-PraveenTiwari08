07b95352524f672cd7db33a5a04264128c7238c4
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-05-05 14:19:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	if(str.includes("@") || str.includes(".")){
+		return true;
+	}else{
+		return false; 
+	}
 }
 
 // Do not change the code below.
